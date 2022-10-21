e0ef3591a6b69b6de2c093530dfd33b3444f0b9a
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-10-21 23:49:46 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,63 @@
-$(function(){
-   console.log('hi,Lev')
-})
+const scrollHeader = () =>{
+
+   const scrol = document.querySelectorAll('.nav-item')
+   console.log(scrol)
+   
+   for(let anchor of scrol){
+      anchor.addEventListener('click',function(event){
+         event.preventDefault();
+         const blockID = anchor.getAttribute('href')
+         document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+         })
+      })
+   }
+
+
+}
+scrollHeader() // Скрол от меню в заголовке
+
+
+const scrollTRF = () =>{
+
+   const scrol = document.querySelectorAll('.btn-trf')
+   console.log(scrol)
+   
+   for(let anchor of scrol){
+      anchor.addEventListener('click',function(event){
+         event.preventDefault();
+         const blockID = anchor.getAttribute('href')
+         document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+         })
+      })
+   }
+   
+
+}
+
+scrollTRF() //Скрол от кнопки "Tre for free"
+
+const scrolON = () => {
+
+   const scrol = document.querySelectorAll('.card-btn')
+   console.log(scrol)
+   
+   for(let anchor of scrol){
+      anchor.addEventListener('click',function(event){
+         event.preventDefault();
+         const blockID = anchor.getAttribute('href')
+         document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+         })
+      })
+   }
+   
+
+}
+
+scrolON() //Скрол от кнопки "Order now"
+
